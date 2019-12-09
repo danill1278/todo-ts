201273b0bb1f883ab0c517eef3aca0ff79aa3301
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-12-09 04:42:24 +0000

    Deploy danill1278/todo-ts to github.com/danill1278/todo-ts.git:gh-pages

---
+++ precache-manifest.41ffc942ac21be2ba2b596bdeeb6badf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "604ba87bb88b850aebce50182fa9e3e5",
+    "url": "/todo-ts/index.html"
+  },
+  {
+    "revision": "ccc602aebda202f86a17",
+    "url": "/todo-ts/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "04286f476c7680f9cf85",
+    "url": "/todo-ts/static/js/2.bf8c04a4.chunk.js"
+  },
+  {
+    "revision": "ccc602aebda202f86a17",
+    "url": "/todo-ts/static/js/main.b4949548.chunk.js"
+  },
+  {
+    "revision": "583e2848ca7c085bf7da",
+    "url": "/todo-ts/static/js/runtime-main.02aa92d1.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.47c8ccb67f18859060d52b3453eee667.js"
+  "/todo-ts/precache-manifest.41ffc942ac21be2ba2b596bdeeb6badf.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/todo-ts/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
