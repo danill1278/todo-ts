99ca535d09b15d5f1382c422c30c4582bb8e24ba
Danil Kolesnik <dankoleman@gmail.com>
2019-12-09 05:48:26 +0300

    remove build from git ignore

---
+++ build/precache-manifest.6cdc0b8d322dacb8f7918d3221d35f82.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b25b087e66790df048b2a0e8841ac16d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "881b5e45c676c94a94cc",
+    "url": "/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "04286f476c7680f9cf85",
+    "url": "/static/js/2.bf8c04a4.chunk.js"
+  },
+  {
+    "revision": "881b5e45c676c94a94cc",
+    "url": "/static/js/main.0afb39f8.chunk.js"
+  },
+  {
+    "revision": "204873f56041f1551616",
+    "url": "/static/js/runtime-main.155c59c5.js"
+  }
+]);
