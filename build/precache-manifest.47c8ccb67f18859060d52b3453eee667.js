f51572f98ad5e4dd2efed90ed93de2e045377a09
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-12-09 03:50:17 +0000

    Deploy danill1278/todo-ts to github.com/danill1278/todo-ts.git:gh-pages

---
+++ build/precache-manifest.47c8ccb67f18859060d52b3453eee667.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6144b6be5466311aa3a2a3f8ec17467",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4ae1d51052a12693d4d9",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "04286f476c7680f9cf85",
+    "url": "/static/js/2.bf8c04a4.chunk.js"
+  },
+  {
+    "revision": "4ae1d51052a12693d4d9",
+    "url": "/static/js/main.0bc3ad65.chunk.js"
+  },
+  {
+    "revision": "204873f56041f1551616",
+    "url": "/static/js/runtime-main.155c59c5.js"
+  }
+]);
